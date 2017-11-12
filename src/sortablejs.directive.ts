bae2d489db21dcc4fd2e7d054fab0dcee99425aa
Balázs Wermer <wermerb@gmail.com>
2017-11-12 13:31:32 +0100

    Revert indentation

--- src/sortablejs.directive.ts
+++ src/sortablejs.directive.ts
@@ -1,19 +1,10 @@
 import {
-  ChangeDetectorRef,
-  Directive,
-  ElementRef,
-  Inject,
-  Input,
-  NgZone,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-  SimpleChange,
-  SimpleChanges
+  Directive, ElementRef, Input, OnInit, OnChanges, OnDestroy, NgZone, SimpleChanges, SimpleChange,
+  ChangeDetectorRef, Inject
 } from '@angular/core';
-import {SortablejsOptions} from './sortablejs-options';
-import {GLOBALS} from './globals';
-import {SortablejsService} from './sortablejs.service';
+import { SortablejsOptions } from './sortablejs-options';
+import { GLOBALS } from './globals';
+import { SortablejsService } from './sortablejs.service';
 
 import * as Sortable from 'sortablejs/Sortable.min';
 
@@ -28,83 +19,18 @@ export class SortablejsDirective implements OnInit, OnChanges, OnDestroy {
   @Input('sortablejsOptions')
   inputOptions: SortablejsOptions;
 
-  @Input() runInsideAngular = false;
-
   private _sortable: any;
 
-  constructor(@Inject(GLOBALS)
-              private globalConfig: SortablejsOptions,
-              private sortablejsService: SortablejsService,
-              private element: ElementRef,
-              private zone: NgZone,
-              private cdr: ChangeDetectorRef) {
-  }
+  @Input() runInsideAngular = false;
 
-  private get options() {
-    return Object.assign({}, this.globalConfig, this.inputOptions, this.overridenOptions);
-  }
-
-  // returns whether the items are currently set
-  private get bindingEnabled() {
-    return !!this.items;
-  }
-
-  // we don't want to have a dependency on @angular/forms just for that
-  private get isItemsFormArray() {
-    // just checking for random FormArray methods not available on a standard array
-    return !!this.items.at && !!this.items.insert && !!this.items.reset;
-  }
-
-  private get overridenOptions(): SortablejsOptions {
-    // always intercept standard events but act only in case items are set (bindingEnabled)
-    // allows to forget about tracking this.items changes
-    return {
-      onAdd: (event: SortableEvent) => {
-        if (this.bindingEnabled) {
-          this.sortablejsService.onremove = (item: any) => {
-            if (this.isItemsFormArray) {
-              this.items.insert(event.newIndex, item);
-            } else {
-              this.items.splice(event.newIndex, 0, item);
-            }
-          };
-        }
-
-        this.proxyEvent('onAdd', event);
-      },
-      onRemove: (event: SortableEvent) => {
-        if (this.bindingEnabled) {
-          let item: any;
-
-          if (this.isItemsFormArray) {
-            item = this.items.at(event.oldIndex);
-            this.items.removeAt(event.oldIndex);
-          } else {
-            item = this.items.splice(event.oldIndex, 1)[0];
-          }
-
-          this.sortablejsService.onremove(item);
-          this.sortablejsService.onremove = null;
-        }
-
-        this.proxyEvent('onRemove', event);
-      },
-      onUpdate: (event: SortableEvent) => {
-        if (this.bindingEnabled) {
-          if (this.isItemsFormArray) {
-            let relocated = this.items.at(event.oldIndex);
-
-            this.items.removeAt(event.oldIndex);
-            this.items.insert(event.newIndex, relocated);
-          } else {
-            this.items.splice(event.newIndex, 0, this.items.splice(event.oldIndex, 1)[0]);
-          }
-        }
-
-        this.proxyEvent('onUpdate', event);
-      }
-    };
-  }
+  constructor(
+    @Inject(GLOBALS)
+    private globalConfig: SortablejsOptions,
+    private sortablejsService: SortablejsService,
+    private element: ElementRef,
+    private zone: NgZone,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   public ngOnInit() {
     if (this.runInsideAngular) {
@@ -130,12 +56,14 @@ export class SortablejsDirective implements OnInit, OnChanges, OnDestroy {
     }
   }
 
-  // we need this to identify that the input is a FormArray
-
   public ngOnDestroy() {
     if (this._sortable) {
       this._sortable.destroy();
     }
+  }
+
+  private get options() {
+    return Object.assign({}, this.globalConfig, this.inputOptions, this.overridenOptions);
   }
 
   private proxyEvent(eventName: string, event: SortableEvent) {
@@ -146,9 +74,69 @@ export class SortablejsDirective implements OnInit, OnChanges, OnDestroy {
     this.cdr.detectChanges();
   }
 
+  // returns whether the items are currently set
+  private get bindingEnabled() {
+    return !!this.items;
+  }
+
+  // we need this to identify that the input is a FormArray
+  // we don't want to have a dependency on @angular/forms just for that
+  private get isItemsFormArray() {
+    // just checking for random FormArray methods not available on a standard array
+    return !!this.items.at && !!this.items.insert && !!this.items.reset;
+  }
+
+  private get overridenOptions(): SortablejsOptions {
+    // always intercept standard events but act only in case items are set (bindingEnabled)
+    // allows to forget about tracking this.items changes
+    return {
+      onAdd: (event: SortableEvent) => {
+        if (this.bindingEnabled) {
+          this.sortablejsService.onremove = (item: any) => {
+            if (this.isItemsFormArray) {
+                this.items.insert(event.newIndex, item);
+            } else {
+                this.items.splice(event.newIndex, 0, item);
+            }
+          };
+        }
+
+        this.proxyEvent('onAdd', event);
+      },
+      onRemove: (event: SortableEvent) => {
+        if (this.bindingEnabled) {
+          let item: any;
+
+          if (this.isItemsFormArray) {
+              item = this.items.at(event.oldIndex);
+              this.items.removeAt(event.oldIndex);
+          } else {
+              item = this.items.splice(event.oldIndex, 1)[0];
+          }
+
+          this.sortablejsService.onremove(item);
+          this.sortablejsService.onremove = null;
+        }
+
+        this.proxyEvent('onRemove', event);
+      },
+      onUpdate: (event: SortableEvent) => {
+        if (this.bindingEnabled) {
+          if (this.isItemsFormArray) {
+            let relocated = this.items.at(event.oldIndex);
+
+            this.items.removeAt(event.oldIndex);
+            this.items.insert(event.newIndex, relocated);
+          } else {
+            this.items.splice(event.newIndex, 0, this.items.splice(event.oldIndex, 1)[0]);
+          }
+        }
+
+        this.proxyEvent('onUpdate', event);
+      }
+    };
+  }
+
 }
 
-interface SortableEvent {
-  oldIndex: number;
-  newIndex: number;
-}
+interface SortableEvent { oldIndex: number; newIndex: number; }
