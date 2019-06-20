d063978ccfb8173b03da6a741a05d24946cf7baf
smnbbrv <smnbbrv@users.noreply.github.com>
2019-06-20 14:02:21 +0200

    test: add basic directive test

---
+++ projects/ngx-sortablejs/src/lib/sortablejs.directive.spec.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { async, TestBed } from '@angular/core/testing';
+import { SortablejsModule } from './sortablejs.module';
+
+describe('SortablejsDirective', () => {
+  @Component({
+    template: `
+      <div [sortablejs]="items">
+        <div *ngFor="let item of items">{{item}}</div>
+      </div>
+    `,
+  })
+  class TestComponent1 {
+    items = [1, 2, 3, 4, 5];
+  }
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TestComponent1,
+      ],
+      imports: [
+        SortablejsModule,
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create', () => {
+    const fixture = TestBed.createComponent(TestComponent1);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
